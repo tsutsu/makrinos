fbf5429deb9315e64e8108b69cb9b485186ea6ba
Levi Aul <levi@leviaul.com>
2018-10-22 17:06:34 -0700

    Update Mix Project for Hex

--- mix.exs
+++ mix.exs
@@ -1,15 +1,35 @@
 defmodule Makrinos.MixProject do
   use Mix.Project
 
-  def project do
-    [
-      app: :makrinos,
-      version: "0.1.0",
-      elixir: "~> 1.7",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
-    ]
+  @version "0.1.0"
+
+  def project, do: [
+    app: :makrinos,
+    version: @version,
+    elixir: "~> 1.7",
+    start_permanent: Mix.env() == :prod,
+    deps: deps(),
+
+    description: description(),
+    package: package(),
+    name: "Makrinos",
+    source_url: "https://github.com/tsutsu/makrinos",
+    docs: docs()
+  ]
+
+  defp description do
+    """
+    An efficient Elixir JSON-RPC client with support for multiple transports
+    """
   end
+
+  defp package, do: [
+    name: :makrinos,
+    files: ["lib", "mix.exs", "README.md", "LICENSE"],
+    maintainers: ["Levi Aul"],
+    licenses: ["MIT"],
+    links: %{"GitHub" => "https://github.com/tsutsu/makrinos"}
+  ]
 
   def application do
     [
@@ -21,7 +41,18 @@ defmodule Makrinos.MixProject do
   defp deps do
     [
       {:jason, "~> 1.1"},
-      {:machine_gun, "~> 0.1.2"}
+      {:machine_gun, "~> 0.1.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
+
+  defp docs, do: [
+    source_ref: "v#\{@version\}",
+    canonical: "https://hexdocs.pm/makrinos",
+    main: "readme",
+    extras: ["README.md"],
+    groups_for_extras: [
+      "Readme": Path.wildcard("*.md")
+    ]
+  ]
 end
